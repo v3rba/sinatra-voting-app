1bcc6aee2019c5a84d579ea64e42206e8b076322
Ronen Verdi <r0nv3r@gmail.com>
2018-09-11 11:47:51 -0400

    delete empty nested folder, added Vote class to vote.rb

--- vote.rb
+++ vote.rb
@@ -1,33 +1,35 @@
 require 'yaml/store'
 require 'sinatra'
 
-get '/' do
-  @title = 'Добро пожаловать!'
-  erb :index
-end
-
-post '/cast' do
-  @title = 'спасибо за голосование!'
-  @vote  = params['vote']
-  @store = YAML::Store.new 'votes.yml'
-  @store.transaction do
-    @store['votes'] ||= {}
-    @store['votes'][@vote] ||= 0
-    @store['votes'][@vote] += 1
+class Vote < Sinatra::Base
+  get '/' do
+    @title = 'Добро пожаловать!'
+    erb :index
   end
-  erb :cast
-end
 
-get '/results' do
-  @title = 'Results so far:'
-  @store = YAML::Store.new 'votes.yml'
-  @votes = @store.transaction { @store['votes'] }
-  erb :results
-end
+  post '/cast' do
+    @title = 'спасибо за голосование!'
+    @vote  = params['vote']
+    @store = YAML::Store.new 'votes.yml'
+    @store.transaction do
+      @store['votes'] ||= {}
+      @store['votes'][@vote] ||= 0
+      @store['votes'][@vote] += 1
+    end
+    erb :cast
+  end
 
-Choices = {
-  'Win' => 'Windows',
-  'OSX' => 'Mac OS',
-  'Min' => 'Linux Mint',
-  'Ubu' => 'Linux Ubuntu',
-}
+  get '/results' do
+    @title = 'Results so far:'
+    @store = YAML::Store.new 'votes.yml'
+    @votes = @store.transaction { @store['votes'] }
+    erb :results
+  end
+
+  Choices = {
+    'Win' => 'Windows',
+    'OSX' => 'Mac OS',
+    'Min' => 'Linux Mint',
+    'Ubu' => 'Linux Ubuntu',
+  }
+end
